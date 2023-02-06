d3faf86a3bb815ccabc1bc483d83c70713aa57e3
Josh0992 <mynorcampos_8@hotmail.com>
2023-02-06 17:53:01 -0600

    se incluye el ejercicio del tema 3 katasEntity.ts donde se incluye el Schema de MongoDB

---
+++ src/domain/entities/Katas.Entity.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const katasEntity = () => {
+    let katasSchema = new mongoose.Schema(
+        {
+            name: String,
+            description: String,
+            level: Number,
+            user: Object,
+            date: Date,
+            valoration: Number,
+            attempts: Number
+        }
+    )
+    return mongoose.model("katas", katasSchema)
+}
